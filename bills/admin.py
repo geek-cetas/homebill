5c6c1ddc9ff1c38f1127288040dbf01983ad6457
Kailash <kailashnathreddy@ymail.com>
2011-05-19 16:08:05 +0530

    added enuf admin ifaces for models

--- bills/admin.py
+++ bills/admin.py
@@ -1,23 +1,69 @@
 from django.contrib import admin
+from django.contrib.admin.filterspecs import FilterSpec, ChoicesFilterSpec
+from django.utils.translation import ugettext_lazy as _
+from django.http import HttpResponse
 from bills.models import *
 
+# Filters -------------------------------------------
+
+class TransactionTypeFilter(ChoicesFilterSpec):
+
+    def __init__(self, f, request, params, model, model_admin):
+        super(TransactionTypeFilter, self).__init__(f, request, params, 
+                                                    model, model_admin)
+
+    def choices(self, cl):
+        print cl, dir(cl)
+        return super(TransactionTypeFilter, self).choices(cl)
+
+FilterSpec.filter_specs.insert(0, (lambda f : \
+        getattr(f, 'is_active_filter', False), TransactionTypeFilter))
+
+class ProofInline(admin.TabularInline):
+    model = Proof
+    raw_id_fields = ('Bill',)
+    max_num = 5
+    fk_name = 'Bill'
+    extra = 2
+
+class TranscationInline(admin.StackedInline):
+    model = Transaction
+    extra = 1
 
 class BillAdmin(admin.ModelAdmin):
-    list_display = ('Company', 'Amount', 'Type', 'attachments')
+    list_filter = ('Merchant',)
+    list_display = ('Merchant', 'transactions',
+                    'attachments')
+    search_fields = ('Merchant',)
+
     fieldsets = (
         ('Organization', {
-            'fields' : ('Company', 'BillId')
+            'fields' : ('Merchant', 'BillId')
             }),
-        ('Payment', {
-            'fields' : ('Amount', 'Type')
-            }),
-        ('Uploads', {
-            'fields' : ('Proofs',)
-            })
         )
+    inlines = [TranscationInline, ProofInline]
 
     def save_model(self, request, obj, form, change):
         obj.User = request.user
-        obj.save()
+        super(BillAdmin, self).save_model(request, obj, form, change)
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('bill_link', 'Amount', 'Type', 'Date', 'Description')
+    list_filter = ('Type', 'Bill')
+    fields = ('Amount', 'Date', 'Type', 'Description')
+    readonly_fields = ('Amount', 'Date', 'Type')
+    list_display_links = ('Amount',)
+
+    def has_add_permission(self, req):
+        return False
+
+    def has_change_permission(self, req, obj=None):
+        return True
+        #return req.user.has_perm('can_modify_transaction')
+
+    def has_delete_permission(self, req, obj=None):
+        return False
 
 admin.site.register(Bill, BillAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+
--- bills/models.py
+++ bills/models.py
@@ -9,25 +9,78 @@ PAYMENT_TYPES = (
     )
 
 class Bill(models.Model):
-    Company = models.CharField(max_length=100)
-    BillId = models.CharField(max_length=100)
-    Amount = models.FloatField()
+    Merchant = models.CharField(max_length=100)
+    BillId = models.CharField(max_length=100,
+                                verbose_name='Bill no')
     User = models.ForeignKey(User)
-    Type = models.CharField(max_length=1, choices = PAYMENT_TYPES,
-                            verbose_name = 'Payment type',
-                            default = 'C')
-    Proofs = models.FileField(null = True, upload_to = 'files')
+
+    def transactions(self):
+        transactions = Transaction.objects.filter(Bill=self)
+        text = ""
+        for trans in transactions:
+            text += "%s<br/>" % trans.link()
+        return text
+
+    transactions.short_description = 'Transactions'
+    transactions.allow_tags = True
+    transactions.is_active_filter = True
 
     def attachments(self):
-        url = str(self.Proofs)
-        filename = url.split('/')[-1]
-        anchor = '<a href="/static/%(url)s"> %(img)s %(link)s </a>'
-        img = '<img src="/static/images/icons/%s.gif" />' % \
-                filename[-3:]
-                
-        return anchor % {'url' : url, 'img' : img,
-                            'link' : filename}
+        proofs = Proof.objects.filter(Bill=self)
+        display_html = ''
+        for proof in proofs:
+            display_html += "%s<br/>" % proof.get_display_html()
+
+        return display_html
 
     attachments.short_description = 'Proofs'
     attachments.allow_tags = True
+
+class Transaction(models.Model):
+    Bill = models.ForeignKey(Bill)
+    Amount = models.FloatField()
+    Date = models.DateField()
+    Type = models.CharField(max_length=1, choices=PAYMENT_TYPES,
+                            default='C')
+    Description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        permissions = (
+                    ('can_modify_transaction', 'Can modify transactions'),
+                    )
+
+    def bill_link(self):
+        bill_url =  '<a href="../bill/%(billid)d">%(merchant)s</a>'
+        return bill_url % {'billid' : self.Bill.id,
+                            'merchant' : self.Bill.Merchant}
+    bill_link.allow_tags = True
+    bill_link.short_description = 'Merchant'
+
+    def link(self):
+        url = '<a href="../transaction/%(transid)d">%(type)s : ' \
+                '%(amount)0.2f</a>'
+        return url % {'transid' : self.id, 
+                        'type' : self.get_Type_display(),
+                        'amount' : self.Amount}
+
+    def __str__(self):
+        transaction_template = "Amount : %(amount)0.2f, Type : %(type)s"
+        return transaction_template % {'amount' : self.Amount, 
+                                        'type' : self.get_Type_display()}
+
+    def __unicode__(self):
+        return self.__str__()
+
+class Proof(models.Model):
+    Bill = models.ForeignKey(Bill)
+    File = models.FileField(null=True, upload_to='files')
+
+    def get_display_html(self):
+        filename = self.File.name.split('/')[-1]
+        url = self.File.url
+        anchor = '<a href="/static/%(url)s"> %(img)s %(link)s </a>'
+        img = '<img src="/static/images/icons/%s.gif" />' % \
+                    filename[-3:]
+        return anchor % {'url' : url, 'img' : img,
+                            'link' : filename}
 
